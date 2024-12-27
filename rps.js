8e8a7877e5446acc721ffb62742d19d2a37820ab
Sinner-Dev95 <sinner-dev95@Xubuntu.myguest.virtualbox.org>
2024-12-27 17:54:22 +0100

    create switch for randomChoice:
    when u call the funct getComputerChoice the
    console generate a random str for the game

--- rps.js
+++ rps.js
@@ -1,5 +1,15 @@
 console.log('Hello World');
 function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3);  // Numero casuale tra 0 e 2
+   switch (randomChoice) {
+    case 0:
+        return 'Rock';
+    case 1:
+        return 'Paper';        
+    case 2:
+        return 'Scissors';        
+        
    
+    
+   }
 }
