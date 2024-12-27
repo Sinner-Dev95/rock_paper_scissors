58289e4926d51c3920338cb7104137b455ad030c
Sinner-Dev95 <sinner-dev95@Xubuntu.myguest.virtualbox.org>
2024-12-27 18:06:25 +0100

    create the function getHumanChoice
    this funct permit to choose between three number
    [0 1 2]
    for choose Rock Paper Scissors.

--- rps.js
+++ rps.js
@@ -1,15 +1,35 @@
-console.log('Hello World');
+console.log("Hello World");
+
+//Funzione per  scelta casuale del computer
+// Creare una funzione per la scelta del computer.
+// Creare al suo interno una variabile che crei un numero     casuale tra 0 e 2
+//creare uno switch che richiami la variabil.
+// per ogni caso dare una scelta differente tra rock, paper and scissors.
+
 function getComputerChoice() {
-    const randomChoice = Math.floor(Math.random() * 3);  // Numero casuale tra 0 e 2
-   switch (randomChoice) {
+  const randomChoice = Math.floor(Math.random() * 3); // Numero casuale tra 0 e 2
+  switch (randomChoice) {
     case 0:
-        return 'Rock';
+      return "Rock";
     case 1:
-        return 'Paper';        
+      return "Paper";
     case 2:
-        return 'Scissors';        
-        
-   
-    
-   }
+      return "Scissors";
+  }
 }
+
+//Funzione per la scelta dell' essere umano
+//Creare switch per scelta tra le tre opzioni
+function getHumanChoice(int) {
+  switch (int) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
+}
+
+//Dichiarare le variabili per tener traccia del punteggio dei giocatori
+
